7f78eb859749ee26208abf0571ea554a7625bb1e
Rajvi Makadiya <rajvimakadiya17@example.com>
2025-04-14 22:15:32 +0530

    login and forget password 2

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__ , static_folder = 'static')
 
 @app.route('/')
 def login():
-    return render_template('auth/signin-2.html')
+    return render_template('auth/signin.html')
 
 @app.route('/forgot')
 def forgot_password():
-    return render_template('auth/forgot-2.html')
+    return render_template('auth/forgot.html')
+
+@app.route('/index')
+def home():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
