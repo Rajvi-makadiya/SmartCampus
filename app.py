e3f425b2641c85f8034c1d7102ab794531415e47
Rajvi Makadiya <rajvimakadiya17@example.com>
2025-04-20 11:57:20 +0530

    changes for auth

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app = Flask(__name__ , static_folder = 'static')
 
 @app.route('/')
@@ -10,9 +9,25 @@ def login():
 def forgot_password():
     return render_template('auth/forgot.html')
 
+@app.route('/reset')
+def reset_password():
+    return render_template('auth/reset-password.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat/chatapp.html')
+
+@app.route('/role')
+def role():
+    return render_template('role/role.html')
+
+@app.route('/team-list')
+def team_list():
+    return render_template('staff/team-list.html')
+
 @app.route('/index')
 def home():
-    return render_template('base.html')
+    return render_template('auth/hy.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
